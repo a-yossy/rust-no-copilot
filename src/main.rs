69c202b0393d6e1522079a0f47b45a0da1bdd685
a-yossy <atsuya0930@yahoo.co.jp>
2025-03-06 12:44:20 +0900

    feat:  `.vscode/settings.json ` 作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::{create_dir_all, File};
+use std::io::Write;
+use std::path::Path;
+
+const COPILOT_DISABLED_SETTING: &str = r#"{
+  "editor.inlineSuggest.enabled": false,
+}
+"#;
+
+fn main() -> std::io::Result<()> {
+    let vscode_dir = Path::new(".vscode");
+    create_dir_all(vscode_dir)?;
+
+    let settings_file = vscode_dir.join("settings.json");
+    let mut file = File::create(settings_file)?;
+    file.write_all(COPILOT_DISABLED_SETTING.as_bytes())?;
+
+    Ok(())
 }
